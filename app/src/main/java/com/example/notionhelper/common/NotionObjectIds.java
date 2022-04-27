1d99b49975db5d70746a321ce62e388d19ed72ac
coachgibbo <shareefabdelal@gmail.com>
2022-04-27 23:25:54 +1000

    Refactored updatedailytasks_fragment.xml into a reusable script_fragment.xml

---
+++ app/src/main/java/com/example/notionhelper/common/NotionObjectIds.java
@@ -0,0 +1,18 @@
+package com.example.notionhelper.common;
+
+public class NotionObjectIds {
+    // Add Daily Task
+    public static final String DAILY_TASK_DATABASE = "xxx";
+
+    // Update Daily Tasks
+    public static final String DAILY_TASK_TEMPLATE = "xxx";
+    public static final String DAILY_TASK_TEMPLATE_2 = "xxx";
+    public static final String DAILY_TASK_TEMPLATE_3 = "xxx";
+    public static final String DAILY_TASK_TEMPLATE_4 = "xxx";
+    public static final String DAILY_TASK_TEMPLATE_5 = "xxx";
+    public static final String DAILY_TASK_TEMPLATE_6 = "xxx";
+    public static final String DAILY_TASK_TEMPLATE_7 = "xxx";
+    public static final String DAILY_TASK_TEMPLATE_8 = "xxx";
+    public static final String DAILY_TASK_TEMPLATE_9 = "xxx";
+
+}
--- app/src/main/java/com/example/notionhelper/view/fragments/UpdateDailyTasksFragment.java
+++ app/src/main/java/com/example/notionhelper/view/fragments/UpdateDailyTasksFragment.java
@@ -1,13 +1,40 @@
 package com.example.notionhelper.view.fragments;
 
-import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.notionhelper.R;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class UpdateDailyTasksFragment extends ItemFragment {
 
+    TextView statusText;
+
     public UpdateDailyTasksFragment() {
-        super(R.layout.updatedailytasks_fragment);
+        super(R.layout.script_fragment);
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        statusText = view.findViewById(R.id.script_status);
+    }
+
+    public ArrayList<String> getInputs() {
+        ArrayList<String> inputs = new ArrayList<>();
+        inputs.add(buildDate());
+
+        return inputs;
+    }
+
+    // Cannot use java.time for this as it requires Android API 26> (Currently 21>)
+    private String buildDate() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
+
+        return formatter.format(Calendar.getInstance().getTime());
+    }
 }
