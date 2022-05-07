2a89c0c3bfb9fd737a7ffce4630cd87b119c0bd1
coachgibbo <shareefabdelal@gmail.com>
2022-05-08 03:01:18 +1000

    Add a UI for next task functionality to AddDailyTaskWidget

--- app/src/main/java/com/example/notionhelper/AddDailyTaskWidget.java
+++ app/src/main/java/com/example/notionhelper/AddDailyTaskWidget.java
@@ -3,6 +3,7 @@ package com.example.notionhelper;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -10,8 +11,14 @@ import android.widget.RemoteViews;
 
 public class AddDailyTaskWidget extends AppWidgetProvider {
 
+    private static final String refreshAction = "refresh";
+    private static final String openAction = "open";
+    private static final String completeAction = "complete";
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        ComponentName addDailyTaskWidget = new ComponentName(context, AddDailyTaskWidget.class);
+
         for (int appWidgetId : appWidgetIds) {
             Intent addDailyTaskIntent = new Intent(context, ItemActivity.class);
             addDailyTaskIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
@@ -23,14 +30,30 @@ public class AddDailyTaskWidget extends AppWidgetProvider {
 
             RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.adddailytask_widget);
             views.setOnClickPendingIntent(R.id.adddailytask_widget_button, pendingAddDailyTaskIntent);
+            views.setOnClickPendingIntent(R.id.adddailytask_widget_refreshbutton, getPendingSelfIntent(context, refreshAction));
+            views.setOnClickPendingIntent(R.id.adddailytask_widget_openbutton, getPendingSelfIntent(context, openAction));
+            views.setOnClickPendingIntent(R.id.adddailytask_widget_completebutton, getPendingSelfIntent(context, completeAction));
 
             appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 
     @Override
-    public void onEnabled(Context context) {}
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
 
-    @Override
-    public void onDisabled(Context context) {}
+        if (refreshAction.equals(intent.getAction())) {
+            Log.i("WIDGETTEST", "Caught the refresh button press");
+        } else if (openAction.equals(intent.getAction())) {
+            Log.i("WIDGETTEST", "Caught the open button press");
+        } else if (completeAction.equals(intent.getAction())) {
+            Log.i("WIDGETTEST", "Caught the complete button press");
+        }
+    }
+
+    private PendingIntent getPendingSelfIntent(Context context, String action) {
+        Intent intent = new Intent(context, this.getClass());
+        intent.setAction(action);
+        return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+    }
 }
